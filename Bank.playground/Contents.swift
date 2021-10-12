f0374983296ac3377ea0f52c85007bee051ab90e
RahafAbdulaal <Rahaf.Abdulaal.med.s@tuwaiq.edu.sa>
2021-10-12 20:31:16 +0300

    trying to do the struct

--- Bank.playground/Contents.swift
+++ Bank.playground/Contents.swift
@@ -1,5 +1,29 @@
 import UIKit
+import Foundation
 
+enum TransactionType {
+    case BillPayment
+    case WireMoney
+}
+struct Transaction {
+    var TransID : Int
+    var TransDate : Date
+    var TransAmount : Int
+    var TransType : TransactionType
+
+mutating func BillPayment(bills : Int){
+    if (bills == 0) {
+        print("The bill is paid")
+              }else {
+        print("Please pay the bill")
+        }
+}
+    mutating func WireMoney(s1 : Bank, s2 :Bank, wire:Float){
+        s1.Balance-=wire
+        s2.Balance+=wire
+        print("")
+    }
+}
 class Bank {
     var bankAccount = [Bank]()
     var FullName : String
@@ -44,33 +68,9 @@ class Bank {
         }
     }
     
-    func BillsPayments (){
-        if self.bills == 0 {
-            print("The bill is paid")
-                  }else {
-            print("Please pay the bill")
-            }
-    }
-    func WireMoney(s1 : Bank, s2 :Bank, wire:Float){
-        s1.Balance-=wire
-        s2.Balance+=wire
-        print("")
-    }
 }
-var b1 = Bank(FullName: "Rahaf Abdulaal", Email: "miss-rofa@live.com", PhoneNumber: 0566700107, UserName: "@RahafAbdulaal", passwords: "R%$67ahaf", bills: 156.50 , Balance: 900.75)
+var b1 = Bank(FullName: "rahaf", Email: "r@r", PhoneNumber: 056666666, UserName: "rahafabdulaal", passwords: "@1fgsy7", bills: 150.0, Balance: 9000.50)
 b1.createAccount(bank: b1)
 b1.viweAccounts()
-b1.BillsPayments()
-b1.viweAccounts()
 
-
-
-var b2 = Bank(FullName: "Gehan Ahmad", Email: "Gege@live.com", PhoneNumber: 0544667971, UserName: "@Gehan", passwords: "G%$67GeGe", bills: 0 , Balance: 10000)
-b2.createAccount(bank: b2)
-b2.viweAccounts()
-b2.BillsPayments()
-
-b1.WireMoney(s1: b1, s2: b2, wire: 50.0)
-b1.GetBalance()
-b2.GetBalance()
 
